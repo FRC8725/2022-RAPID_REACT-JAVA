eaddcbb3667e54aaa93cc1416649043193f3bf23
ヾ(Ő∀Ő3)ノ <est88>
2022-02-09 11:28:26 +0800

    PID : (

--- src/main/java/frc/robot/subsystem/AutoShoot.java
+++ src/main/java/frc/robot/subsystem/AutoShoot.java
@@ -1,19 +1,24 @@
 package frc.robot.subsystem;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
 
 public class AutoShoot extends DriveSub {
     // This class is for AutoShoot.
     // including RotationPID, ShootSpeedController
 
     Odometry Odometry = new Odometry();
+    
 
     public void Aim(double angle) {
         double error = 0 - angle;
-        double kp = 1 / 45.f;
-        var speed = MathUtil.clamp(kp * error, -1, 1);
-        Move(kp * speed, -speed);
+        double kp = 1 / 46.;
+        double speed = kp * error;
+        speed = MathUtil.clamp(speed, -0.9, 0.9);
+        SmartDashboard.putNumber("diffspeed", speed);
+        Move(speed, -speed);
     }
 
 }
+
