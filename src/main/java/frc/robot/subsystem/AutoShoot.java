ebbac61e5f8f62142fd585dae837098b57dee970
Noodle4219005 <mbdd0921@gmail.com>
2022-02-09 11:45:09 +0800

    PID

--- src/main/java/frc/robot/subsystem/AutoShoot.java
+++ src/main/java/frc/robot/subsystem/AutoShoot.java
@@ -9,16 +9,32 @@ public class AutoShoot extends DriveSub {
     // including RotationPID, ShootSpeedController
 
     Odometry Odometry = new Odometry();
-    
+    PIDController o_pid;
+
+    public AutoShoot() {
+        SmartDashboard.putNumber("Aim_kp", 0);
+        SmartDashboard.putNumber("Aim_ki", 0);
+        SmartDashboard.putNumber("Aim_kd", 0);
+        SmartDashboard.putNumber("min_Integral", 0);
+        SmartDashboard.putNumber("max_Integral", 0);
+        o_pid = new PIDController(0, 0, 0);
+    }
+
+    double kp, ki, kd, min_Integral, max_Integral;
 
     public void Aim(double angle) {
-        double error = 0 - angle;
-        double kp = 1 / 46.;
-        double speed = kp * error;
+        kp = SmartDashboard.getNumber("Aim_kp", 0);
+        ki = SmartDashboard.getNumber("Aim_ki", 0);
+        kd = SmartDashboard.getNumber("Aim_kd", 0);
+        min_Integral = SmartDashboard.getNumber("min_Integral", 0);
+        max_Integral = SmartDashboard.getNumber("max_Integral", 0);
+        o_pid.setPID(kp, ki, kd);
+        o_pid.setTolerance(2);
+        o_pid.setIntegratorRange(min_Integral, max_Integral);
+        double speed = o_pid.calculate(angle, 0);
         speed = MathUtil.clamp(speed, -0.9, 0.9);
         SmartDashboard.putNumber("diffspeed", speed);
         Move(speed, -speed);
     }
 
 }
-
