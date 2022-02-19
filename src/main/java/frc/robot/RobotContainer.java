8b73c8e6354023fc4737dfc8e29a319faf4f8c19
J40026702 <eric.chieng1228@gmail.com>
2022-02-19 15:05:50 +0800

    k

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,23 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.DriveSubsystem;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
-public class RobotContainer {
+public class RobotContainer  extends TimedRobot{
+  DriveSubsystem m_DriveSubsystem =new DrveSubsystem();
   // The robot's subsystems and commands are defined here...
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -23,13 +30,20 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
+  
+
+
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    Joystick Joystick = new Joystick(Constants.Joystick.JOYSTICK_A);
+
+    JoystickButton HalfSpeed = new JoystickButton(Joystick, 7);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -37,6 +51,8 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
+    return new InstantCommand(() -> m_DriveSubsystem.setVoltage(0, 0));
+
     // An ExampleCommand will run in autonomous
   }
 }
