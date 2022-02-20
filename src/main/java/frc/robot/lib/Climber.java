9f5877e77902a63311ee11c4ebf79cc4768cd47e
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2022-02-20 17:45:53 +0800

    Climber 函式更新

--- src/main/java/frc/robot/lib/Climber.java
+++ src/main/java/frc/robot/lib/Climber.java
@@ -3,17 +3,36 @@ package frc.robot.lib;
 import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Climber {
 
-    public Climber() {
+    private WPI_VictorSPX Winch_VictorPG; // 馬達
 
+
+    public Climber() {
+        Winch_VictorPG = new WPI_VictorSPX(Constants.Climber.WINCH_VICTORPG);
     }
 
-    public void Climb() {
+    // 伸縮
+    public void Climb_Up(boolean run) {
+        if (run) {
+            Winch_VictorPG.set(Constants.Climber.WINCH_ENCODER_SPEED);
+        } else {
+            Winch_VictorPG.set(0);
+        }
+    }
 
+    public void Climb_Down(boolean run) {
+        if (run) {
+            Winch_VictorPG.set(- Constants.Climber.WINCH_ENCODER_SPEED);
+        } else {
+            Winch_VictorPG.set(0);
+        }
+    }
+
+    public void Stop() {
+        Winch_VictorPG.set(0);
     }
 
 }
